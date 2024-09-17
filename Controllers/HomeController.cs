9163be84d369f192f89497c3bdf8bc2f148fa4ef
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2024-09-17 23:17:28 +0530

    MCQ

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,25 +4,35 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        ViewData["Title"] = "Home";
+        ViewData["Title"] = "Start your better Learning from here";
         return View();
     }
 
     public IActionResult Topic1()
     {
-        ViewData["Title"] = "Topic 1";
+        ViewData["Title"] = "Azure Topics";
         return View();
     }
 
     public IActionResult Topic2()
     {
-        ViewData["Title"] = "Topic 2";
+        ViewData["Title"] = "C#";
         return View();
     }
 
     public IActionResult Topic3()
     {
-        ViewData["Title"] = "Topic 3";
+        ViewData["Title"] = "Asp.net";
         return View();
     }
+
+    // Action for loading the Azure DevOps content
+    public IActionResult AzureDevOps()
+    {
+        return View(); // This will render a partial view for the content
+    }
+    public IActionResult CloudCompute() { return View(); }
+
+    public IActionResult AzurePaas() { return View(); }
+    
 }
