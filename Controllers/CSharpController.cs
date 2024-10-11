59b16ba7013eb66cd7b808892b4d0f43890109ce
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2024-10-11 21:50:56 +0530

    Leftmenu Not collapse

---
+++ Controllers/CSharpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenieWeb.Controllers
+{
+    public class CSharpController : Controller
+    {
+        public IActionResult IntroductionToCSharp()
+        {
+            return View();
+        }
+    }
+}
