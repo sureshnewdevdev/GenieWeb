f46e0e75d5aadf884c3d55b1eddb3e2ed3a15148
Murali <maluvadumurali@gmail.com>
2024-10-13 14:21:51 +0530

    Upto Data types

--- Controllers/CSharpController.cs
+++ Controllers/CSharpController.cs
@@ -4,23 +4,26 @@ namespace GenieWeb.Controllers
 {
     public class CSharpController : Controller
     {
-        public IActionResult IntroductionToCSharp()
-        {
-            return View();
-        }
+        public IActionResult IntroductionToCSharp() => View();        
         public IActionResult FeatureOfCSharp() => View();
         public IActionResult CSharpCompliationAndExecution() => View();
-
         public IActionResult GeneralStructureOfCSharp() =>View();
-
         public IActionResult CreatingAndUsingDLL() => View();
-
         public IActionResult DataTypesAndArrays() => View();
-
         public IActionResult ValueAndReferenceType() => View();
-
         public IActionResult BoxingUnBoxing() => View();    
         public IActionResult TypeOfArrays() => View();  
         public IActionResult NullableTypes() => View();
+        public IActionResult ImplicitTypeLocalvariables() => View();
+        public IActionResult VarvsDynamic() => View();
+        public IActionResult IsAndAsOperator() => View();
+        public IActionResult RefVsOutKeywords() => View();
+        public IActionResult ObjectBaseClass() => View();    
+        public IActionResult EqualsVsEqual() => View();
+        public IActionResult StringVsStringBuilder() => View(); 
+        public IActionResult VariousStringMethods() => View();
+        public IActionResult DefaultNamedParameters() => View();  
+        public IActionResult ParseTryParseVsConvert() => View();
+
     }
 }
