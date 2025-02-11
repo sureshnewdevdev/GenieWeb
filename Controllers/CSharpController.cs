1049becd46174eabea13308fdd063f54f0823998
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2025-02-11 16:24:04 +0530

    mm

--- Controllers/CSharpController.cs
+++ Controllers/CSharpController.cs
@@ -2,30 +2,31 @@
 
 namespace GenieWeb.Controllers
 {
-public class CSharpController : Controller
-{
-    public IActionResult AbstractClasses()
+    public class CSharpController : Controller
     {
-        ViewData["ActiveMenu"] = "AbstractClasses";
-        return View("AbstractClasses");
-    }
+        public IActionResult AbstractClasses()
+        {
+            ViewData["ActiveMenu"] = "AbstractClasses";
+            return View("AbstractClasses");
+        }
 
-    public IActionResult AccessModifier()
-    {
-        ViewData["ActiveMenu"] = "AccessModifier";
-        return View("/CSharp/AccessModifier");
-    }
+        public IActionResult AccessModifier()
+        {
+            ViewData["ActiveMenu"] = "AccessModifier";
+            return View("/CSharp/AccessModifier");
+        }
 
-    public IActionResult AccessModifiers()
-    {
-        ViewData["ActiveMenu"] = "AccessModifiers";
-        return View("AccessModifiers");
-    }
+        public IActionResult AccessModifiers()
+        {
+            ViewData["ActiveMenu"] = "AccessModifiers";
+            return View("AccessModifiers");
+        }
 
-    public IActionResult IntroductionToCSharp()
-    {
-        ViewData["ActiveMenu"] = "IntroductionToCSharp";
-        return View("IntroductionToCSharp");
+        public IActionResult IntroductionToCSharp()
+        {
+            ViewData["ActiveMenu"] = "IntroductionToCSharp";
+            return View("IntroductionToCSharp");
+        }
+
     }
-    
 }
