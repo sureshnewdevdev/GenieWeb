f5629aa1737bfd85e80f2c94caf9e8dc6301edad
Sriram Elluru <ellurisriram27655@gmail.com>
2025-03-13 14:18:08 +0530

    Added terraform and minor css changes

---
+++ Controllers/TerraformController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenieWeb.Controllers
+{
+    public class TerraformController : Controller
+    {
+        public IActionResult IntroductionToTerraform()
+        {
+            ViewData["ActiveMenu"] = "Terraform";
+            ViewData["ActivePage"] = "IntroductionToTerraform";
+            return View("IntroductionToTerraform");
+        }
+    }
+}
