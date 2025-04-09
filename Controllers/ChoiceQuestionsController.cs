60b764bee6388ed2a1791bef8b66184bc6a0069b
sriram2883 <ellurisriram27655@gmail.com>
2025-04-10 00:22:15 +0530

    Design changes

--- Controllers/ChoiceQuestionsController.cs
+++ Controllers/ChoiceQuestionsController.cs
@@ -1,39 +1,81 @@
+// ChoiceQuestionsController.cs
+using GenieWeb.Helpers;
+using GenieWeb.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GenieWeb.Controllers
 {
     public class ChoiceQuestionsController : Controller
     {
-        public IActionResult Index()
+        private readonly QuizService _quizService;
+        private const string QuizSessionKey = "QuizAnswers";
+
+        public ChoiceQuestionsController(QuizService quizService)
         {
-            ViewData["ActiveMenu"] = "ChoiceQuestions";
-            return View("Index");
-        }
-        public IActionResult AzureStorage()
-        {
-            ViewData["ActiveMenu"] = "ChoiceQuestions";
-            ViewData["ActivePage"] = "AzureStorage";
-            return View("AzureStorage");
+            _quizService = quizService;
         }
 
-        public IActionResult AzureSubtopics()
+        // GET: Show question by topic and number
+        [Route("ChoiceQuestions/{topic}/{qNum?}")]
+        public IActionResult Index(string topic, int qNum = 1)
         {
-            return View("AzureSubtopics");
+            var questions = _quizService.LoadQuestions(topic);
+            if (qNum < 1 || qNum > questions.Count)
+                return RedirectToAction("Index", new { topic, qNum = 1 });
+
+            ViewData["Topic"] = topic;
+            ViewData["QuestionNumber"] = qNum;
+            ViewData["TotalQuestions"] = questions.Count;
+
+            var answers = HttpContext.Session.Get<Dictionary<int, string>>(QuizSessionKey) ?? new();
+            ViewData["SelectedAnswer"] = answers.ContainsKey(qNum) ? answers[qNum] : null;
+
+            return View("QuestionView", questions[qNum - 1]);
         }
 
-        public IActionResult CSharpSubtopics()
+        // POST: Store selected answer and move to next or finish
+        [HttpPost("ChoiceQuestions/SubmitAnswer")]
+        public IActionResult SubmitAnswer(string topic, int questionNumber, string selectedAnswer)
         {
-            return View("CSharpSubtopics");
+            var answers = HttpContext.Session.Get<Dictionary<int, string>>(QuizSessionKey) ?? new();
+            answers[questionNumber] = selectedAnswer;
+            HttpContext.Session.Set(QuizSessionKey, answers);
+
+            var questions = _quizService.LoadQuestions(topic);
+            if (questionNumber >= questions.Count)
+                return RedirectToAction("Score", new { topic });
+
+            return RedirectToAction("Index", new { topic, qNum = questionNumber + 1 });
         }
 
-        public IActionResult DotNetSubtopics()
+        // Final score view
+        [Route("ChoiceQuestions/{topic}/Score")]
+        public IActionResult Score(string topic)
         {
-            return View("DotNetSubtopics");
-        }
+            var questions = _quizService.LoadQuestions(topic);
+            var answers = HttpContext.Session.Get<Dictionary<int, string>>(QuizSessionKey) ?? new();
 
-        public IActionResult UnderDevelopment()
-        {
-            return View();
+            int score = 0;
+            for (int i = 0; i < questions.Count; i++)
+            {
+                if (answers.ContainsKey(i + 1) && !string.IsNullOrEmpty(answers[i + 1]))
+                {
+                    var correctIndex = questions[i].CorrectAnswer[0] - 'A';
+                    var selectedIndex = answers[i + 1][0] - 'A';
+
+                    if (correctIndex >= 0 && correctIndex < questions[i].Options.Count &&
+                        selectedIndex >= 0 && selectedIndex < questions[i].Options.Count &&
+                        questions[i].Options[correctIndex] == questions[i].Options[selectedIndex])
+                    {
+                        score += 5;
+                    }
+                }
+            }
+
+            HttpContext.Session.Remove(QuizSessionKey);
+
+            return View("Score", (score, questions.Count * 5, topic, questions, answers));
         }
 
     }
---
+++ Models/QuizQuestion.cs
@@ -0,0 +1,11 @@
+﻿namespace GenieWeb.Models
+{
+    public class QuizQuestion
+    {
+        public string QuestionText { get; set; }
+        public List<string> Options { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string Explanation { get; set; }
+    }
+
+}
