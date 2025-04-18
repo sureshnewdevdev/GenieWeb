8b1eb6a2e6be4dc04e4220a77777fe351f500608
sriram2883 <ellurisriram27655@gmail.com>
2025-04-18 15:39:19 +0530

    datbricks

---
+++ Controllers/DataBricks.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenieWeb.Controllers
+{
+    public class DataBricks : Controller
+    {
+        public IActionResult IntroductionToDataBricks()
+        {
+            return View();
+        }
+    }
+}
